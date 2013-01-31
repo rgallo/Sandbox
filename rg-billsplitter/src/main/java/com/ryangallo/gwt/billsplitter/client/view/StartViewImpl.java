5d3665e61cf3e54a1d33d3f886030490b03a2520
Ryan Gallo <ryan.j.gallo@gmail.com>
2013-01-30 23:42:53 -0500

    Added basic design for start page/view

--- rg-billsplitter/src/main/java/com/ryangallo/gwt/billsplitter/client/view/StartViewImpl.java
+++ rg-billsplitter/src/main/java/com/ryangallo/gwt/billsplitter/client/view/StartViewImpl.java
@@ -1,17 +1,21 @@
 package com.ryangallo.gwt.billsplitter.client.view;
 
-import com.google.gwt.user.client.Window;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
-import com.ryangallo.gwtsafe.utils.StringUtil;
+import com.google.gwt.user.client.ui.Widget;
 
 public class StartViewImpl extends Composite implements StartView {
+
+	private static StartViewImplUiBinder uiBinder = GWT.create(StartViewImplUiBinder.class);
+
+	interface StartViewImplUiBinder extends UiBinder<Widget, StartViewImpl> {
+	}
 
 	private Presenter presenter;
 
 	public StartViewImpl() {
-		initWidget(new Label("Hello World!"));
-		Window.alert("" + StringUtil.isNotBlank("this"));
+		initWidget(uiBinder.createAndBindUi(this));
 	}
 
 	@Override
